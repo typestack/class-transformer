e0558524b984e8e824d6a153816d99b6558ced6c
Hossein Sadeghi <ho.mysterious@gmail.com>
2019-05-26 21:59:30 +0430

    fix: error when object contains promise field

--- src/TransformOperationExecutor.ts
+++ src/TransformOperationExecutor.ts
@@ -1,7 +1,7 @@
 import { defaultMetadataStorage } from './storage';
 import { TypeHelpOptions, TypeOptions, ClassTransformOptions, TypeMetadata } from './interfaces';
 import { TransformationType } from './enums';
-import { getGlobal } from './utils';
+import { getGlobal, isPromise } from './utils';
 
 function instantiateArrayType(arrayType: Function): Array<any> | Set<any> {
   const array = new (arrayType as any)();
@@ -64,6 +64,7 @@ export class TransformOperationExecutor {
               if (!targetType.options.keepDiscriminatorProperty)
                 delete subValue[targetType.options.discriminator.property];
             }
+
             if (this.transformationType === TransformationType.CLASS_TO_CLASS) {
               realTargetType = subValue.constructor;
             }
@@ -116,6 +117,13 @@ export class TransformOperationExecutor {
     } else if (!!getGlobal().Buffer && (targetType === Buffer || value instanceof Buffer) && !isMap) {
       if (value === null || value === undefined) return value;
       return Buffer.from(value);
+    } else if (isPromise(value) && !isMap) {
+      return new Promise((resolve, reject) => {
+        value.then(
+          (data: any) => resolve(this.transform(undefined, data, targetType, undefined, undefined, level + 1)),
+          reject
+        );
+      });
     } else if (typeof value === 'object' && value !== null) {
       // try to guess the type
       if (!targetType && value.constructor !== Object /* && TransformationType === TransformationType.CLASS_TO_PLAIN*/)
