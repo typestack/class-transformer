7e06bfe35cd7416c84d2f4aa6fdcd44ee5e79937
Petr Zahradnik <junior@zahradnik.cz>
2021-09-22 18:23:01 +0200

    fix: use global scope for metadata storage

--- src/storage.ts
+++ src/storage.ts
@@ -1,6 +1,13 @@
 import { MetadataStorage } from './MetadataStorage';
+import { getGlobal } from './utils';
+
+const globalScope = getGlobal();
 
 /**
  * Default metadata storage is used as singleton and can be used to storage all metadatas.
  */
-export const defaultMetadataStorage = new MetadataStorage();
+if (!globalScope.classTransformerMetadataStorage) {
+  globalScope.classTransformerMetadataStorage = new MetadataStorage();
+}
+
+export const defaultMetadataStorage = globalScope.classTransformerMetadataStorage;
