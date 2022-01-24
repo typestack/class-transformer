797ea8159a68d63a4155b12096d24369e9724867
Jannik <jannik@jannikmewes.de>
2022-01-24 17:44:42 +0100

    Export getGlobal + defaultMetadataStorage

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { ClassTransformOptions } from './interfaces';
 import { ClassConstructor } from './interfaces';
 
 export { ClassTransformer } from './ClassTransformer';
+
+export { getGlobal } from './utils';
+export { defaultMetadataStorage } from './storage';
+
 export * from './decorators';
 export * from './interfaces';
 export * from './enums';
