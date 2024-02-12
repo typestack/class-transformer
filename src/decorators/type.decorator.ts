9b0aa686ea2b9086f1ce28a0e4eb11a05c0bc634
DongHyun Kim <temeddix@gmail.com>
2024-02-12 17:11:03 +0900

    Remove old decorator

--- src/decorators/type.decorator.ts
+++ src/decorators/type.decorator.ts
@@ -23,27 +23,3 @@ export function Nested(classConstructor: new (...args: any[]) => any, options: T
     });
   };
 }
-
-/**
- * Specifies a type of the property.
- * The given TypeFunction can return a constructor. A discriminator can be given in the options.
- *
- * Can be applied to properties only.
- *
- * @deprecated Use `@Nested` decorator instead.
- */
-export function Type(
-  typeFunction: (...args: any[]) => new (...args: any[]) => any,
-  options: TypeOptions = {}
-): PropertyDecorator {
-  return function (target: any, propertyName: string | Symbol): void {
-    const reflectedType = (Reflect as any).getMetadata('design:type', target, propertyName);
-    defaultMetadataStorage.addTypeMetadata({
-      target: target.constructor,
-      propertyName: propertyName as string,
-      reflectedType,
-      classConstructor: typeFunction(),
-      options,
-    });
-  };
-}
